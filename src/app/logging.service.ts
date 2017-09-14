6aaf20570522643e916c16ebb78c2df4f0f9ee94
Adnan Mama <mamaa@ugcorp.com>
2017-09-14 15:11:51 -0400

    finishes lecture 102 section 9

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,10 +1,4 @@
-import {Injectable} from '@angular/core';
-
-@Injectable()
 export class LoggingService {
-
-  constructor() {
-  }
 
   logStatusChange(status: string) {
     console.log('A server status changed, new status: ' + status);
